501f40b0313f500d022a75d5b64b6f6f01c0d1ea
Ricardo Reis <ricardomarina.reis@gmail.com>
2025-06-26 20:46:01 -0300

    acerto dos textbox

--- APP-RECEITAS/frmUnidades.Designer.vb
+++ APP-RECEITAS/frmUnidades.Designer.vb
@@ -60,39 +60,45 @@ Partial Class frmUnidades
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 22)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(665, 128)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.GroupBox1.Size = New System.Drawing.Size(1219, 236)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "UNIDADE"
         '
         'txtAbreviacao
         '
-        Me.txtAbreviacao.Location = New System.Drawing.Point(117, 92)
+        Me.txtAbreviacao.Location = New System.Drawing.Point(214, 170)
+        Me.txtAbreviacao.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.txtAbreviacao.Name = "txtAbreviacao"
-        Me.txtAbreviacao.Size = New System.Drawing.Size(47, 22)
+        Me.txtAbreviacao.Size = New System.Drawing.Size(83, 22)
         Me.txtAbreviacao.TabIndex = 5
         '
         'txtDescricao
         '
-        Me.txtDescricao.Location = New System.Drawing.Point(117, 64)
+        Me.txtDescricao.Location = New System.Drawing.Point(214, 118)
+        Me.txtDescricao.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.txtDescricao.Name = "txtDescricao"
-        Me.txtDescricao.Size = New System.Drawing.Size(373, 22)
+        Me.txtDescricao.Size = New System.Drawing.Size(680, 22)
         Me.txtDescricao.TabIndex = 4
         '
         'txtCodigo
         '
         Me.txtCodigo.Enabled = False
-        Me.txtCodigo.Location = New System.Drawing.Point(117, 32)
+        Me.txtCodigo.Location = New System.Drawing.Point(214, 59)
+        Me.txtCodigo.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.txtCodigo.Name = "txtCodigo"
-        Me.txtCodigo.Size = New System.Drawing.Size(47, 22)
+        Me.txtCodigo.Size = New System.Drawing.Size(53, 22)
         Me.txtCodigo.TabIndex = 3
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(16, 95)
+        Me.Label3.Location = New System.Drawing.Point(29, 175)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(91, 16)
         Me.Label3.TabIndex = 2
@@ -101,7 +107,8 @@ Partial Class frmUnidades
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(16, 67)
+        Me.Label2.Location = New System.Drawing.Point(29, 124)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(82, 16)
         Me.Label2.TabIndex = 1
@@ -110,7 +117,8 @@ Partial Class frmUnidades
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(16, 35)
+        Me.Label1.Location = New System.Drawing.Point(29, 65)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(61, 16)
         Me.Label1.TabIndex = 0
@@ -122,9 +130,11 @@ Partial Class frmUnidades
         Me.GroupBox2.Controls.Add(Me.dgvUnidades)
         Me.GroupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 163)
+        Me.GroupBox2.Location = New System.Drawing.Point(22, 301)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(665, 236)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.GroupBox2.Size = New System.Drawing.Size(1219, 436)
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "RELAÇÃO UNIDADES"
@@ -133,9 +143,10 @@ Partial Class frmUnidades
         '
         Me.dgvUnidades.BackgroundColor = System.Drawing.SystemColors.ButtonFace
         Me.dgvUnidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvUnidades.Location = New System.Drawing.Point(6, 25)
+        Me.dgvUnidades.Location = New System.Drawing.Point(11, 46)
+        Me.dgvUnidades.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.dgvUnidades.Name = "dgvUnidades"
-        Me.dgvUnidades.Size = New System.Drawing.Size(653, 205)
+        Me.dgvUnidades.Size = New System.Drawing.Size(1197, 378)
         Me.dgvUnidades.TabIndex = 0
         '
         'GroupBox3
@@ -153,9 +164,11 @@ Partial Class frmUnidades
         Me.GroupBox3.Controls.Add(Me.btnCadastrar)
         Me.GroupBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox3.Location = New System.Drawing.Point(697, 12)
+        Me.GroupBox3.Location = New System.Drawing.Point(1278, 22)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(153, 387)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.GroupBox3.Size = New System.Drawing.Size(280, 714)
         Me.GroupBox3.TabIndex = 5
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "COMANDOS"
@@ -163,9 +176,10 @@ Partial Class frmUnidades
         'btnUltimo
         '
         Me.btnUltimo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnUltimo.Location = New System.Drawing.Point(104, 333)
+        Me.btnUltimo.Location = New System.Drawing.Point(191, 615)
+        Me.btnUltimo.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnUltimo.Name = "btnUltimo"
-        Me.btnUltimo.Size = New System.Drawing.Size(32, 32)
+        Me.btnUltimo.Size = New System.Drawing.Size(59, 59)
         Me.btnUltimo.TabIndex = 9
         Me.btnUltimo.Text = ">|"
         Me.btnUltimo.UseVisualStyleBackColor = True
@@ -173,27 +187,30 @@ Partial Class frmUnidades
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.APP_RECEITAS.My.Resources.Resources.RECEITAS
-        Me.PictureBox1.Location = New System.Drawing.Point(30, 35)
+        Me.PictureBox1.Location = New System.Drawing.Point(55, 65)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(89, 93)
+        Me.PictureBox1.Size = New System.Drawing.Size(163, 172)
         Me.PictureBox1.TabIndex = 6
         Me.PictureBox1.TabStop = False
         '
         'btnProximo
         '
         Me.btnProximo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnProximo.Location = New System.Drawing.Point(75, 333)
+        Me.btnProximo.Location = New System.Drawing.Point(138, 615)
+        Me.btnProximo.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnProximo.Name = "btnProximo"
-        Me.btnProximo.Size = New System.Drawing.Size(32, 32)
+        Me.btnProximo.Size = New System.Drawing.Size(59, 59)
         Me.btnProximo.TabIndex = 8
         Me.btnProximo.Text = ">"
         Me.btnProximo.UseVisualStyleBackColor = True
         '
         'btnCarregaDados
         '
-        Me.btnCarregaDados.Location = New System.Drawing.Point(18, 295)
+        Me.btnCarregaDados.Location = New System.Drawing.Point(33, 545)
+        Me.btnCarregaDados.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnCarregaDados.Name = "btnCarregaDados"
-        Me.btnCarregaDados.Size = New System.Drawing.Size(118, 32)
+        Me.btnCarregaDados.Size = New System.Drawing.Size(216, 59)
         Me.btnCarregaDados.TabIndex = 4
         Me.btnCarregaDados.Text = "Carrega Dados"
         Me.btnCarregaDados.UseVisualStyleBackColor = True
@@ -201,18 +218,20 @@ Partial Class frmUnidades
         'btnAnterior
         '
         Me.btnAnterior.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAnterior.Location = New System.Drawing.Point(46, 333)
+        Me.btnAnterior.Location = New System.Drawing.Point(84, 615)
+        Me.btnAnterior.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnAnterior.Name = "btnAnterior"
-        Me.btnAnterior.Size = New System.Drawing.Size(32, 32)
+        Me.btnAnterior.Size = New System.Drawing.Size(59, 59)
         Me.btnAnterior.TabIndex = 7
         Me.btnAnterior.Text = "<"
         Me.btnAnterior.UseVisualStyleBackColor = True
         '
         'btnLocalizar
         '
-        Me.btnLocalizar.Location = New System.Drawing.Point(18, 257)
+        Me.btnLocalizar.Location = New System.Drawing.Point(33, 474)
+        Me.btnLocalizar.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnLocalizar.Name = "btnLocalizar"
-        Me.btnLocalizar.Size = New System.Drawing.Size(118, 32)
+        Me.btnLocalizar.Size = New System.Drawing.Size(216, 59)
         Me.btnLocalizar.TabIndex = 3
         Me.btnLocalizar.Text = "Localizar"
         Me.btnLocalizar.UseVisualStyleBackColor = True
@@ -220,49 +239,54 @@ Partial Class frmUnidades
         'btnPrimeiro
         '
         Me.btnPrimeiro.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnPrimeiro.Location = New System.Drawing.Point(18, 333)
+        Me.btnPrimeiro.Location = New System.Drawing.Point(33, 615)
+        Me.btnPrimeiro.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnPrimeiro.Name = "btnPrimeiro"
-        Me.btnPrimeiro.Size = New System.Drawing.Size(32, 32)
+        Me.btnPrimeiro.Size = New System.Drawing.Size(59, 59)
         Me.btnPrimeiro.TabIndex = 6
         Me.btnPrimeiro.Text = "|<"
         Me.btnPrimeiro.UseVisualStyleBackColor = True
         '
         'btnDeletar
         '
-        Me.btnDeletar.Location = New System.Drawing.Point(18, 219)
+        Me.btnDeletar.Location = New System.Drawing.Point(33, 404)
+        Me.btnDeletar.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnDeletar.Name = "btnDeletar"
-        Me.btnDeletar.Size = New System.Drawing.Size(118, 32)
+        Me.btnDeletar.Size = New System.Drawing.Size(216, 59)
         Me.btnDeletar.TabIndex = 2
         Me.btnDeletar.Text = "Deletar"
         Me.btnDeletar.UseVisualStyleBackColor = True
         '
         'btnAlterar
         '
-        Me.btnAlterar.Location = New System.Drawing.Point(18, 181)
+        Me.btnAlterar.Location = New System.Drawing.Point(33, 334)
+        Me.btnAlterar.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnAlterar.Name = "btnAlterar"
-        Me.btnAlterar.Size = New System.Drawing.Size(118, 32)
+        Me.btnAlterar.Size = New System.Drawing.Size(216, 59)
         Me.btnAlterar.TabIndex = 1
         Me.btnAlterar.Text = "Alterar"
         Me.btnAlterar.UseVisualStyleBackColor = True
         '
         'btnCadastrar
         '
-        Me.btnCadastrar.Location = New System.Drawing.Point(18, 143)
+        Me.btnCadastrar.Location = New System.Drawing.Point(33, 264)
+        Me.btnCadastrar.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.btnCadastrar.Name = "btnCadastrar"
-        Me.btnCadastrar.Size = New System.Drawing.Size(118, 32)
+        Me.btnCadastrar.Size = New System.Drawing.Size(216, 59)
         Me.btnCadastrar.TabIndex = 0
         Me.btnCadastrar.Text = "Novo"
         Me.btnCadastrar.UseVisualStyleBackColor = True
         '
         'frmUnidades
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
-        Me.ClientSize = New System.Drawing.Size(862, 411)
+        Me.ClientSize = New System.Drawing.Size(1370, 749)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.Name = "frmUnidades"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "UNIDADES"
